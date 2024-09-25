a7808a0e35f019da7885e9bb5b07a22250c1b5b9
shamodhasahan <shamodhasrathnamalala@gmail.com>
2024-09-25 13:18:59 +0530

    initial commit

---
+++ src/main/java/ijse/gdse/navigations/AppInitializer.java
@@ -0,0 +1,27 @@
+package ijse.gdse.navigations;
+
+/**
+ * --------------------------------------------
+ * Author: R.I.B. Shamodha Sahan Rathnamalala
+ * GitHub: https://github.com/shamodhas
+ * Website: https://shamodha.live
+ * --------------------------------------------
+ * Created: 9/25/2024 12:22 PM
+ * Project: Navigations
+ * --------------------------------------------
+ **/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ijse.gdse.navigations {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ijse.gdse.navigations to javafx.fxml;
+    exports ijse.gdse.navigations;
+}
